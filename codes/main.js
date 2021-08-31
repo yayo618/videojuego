f9edf48c4e8d534677a713bb1cf6cbc48954522c
yayo <jorgefloresari@gmail.com>
2021-08-31 20:37:05 +0000

    my-canv gira en fullscreen

--- codes/main.js
+++ codes/main.js
@@ -1,6 +1,26 @@
 var cnv = document.getElementById("my-canv");
 var ctx = cnv.getContext("2d");
 
+var elem = document.documentElement;
+if (window.innerWidth > window.innerHeight) {
+	fullscreenOpen();
+//	screen.orientation.lock('landscape');
+} else {alert("portrait");}
+function fullscreenOpen () {
+//    cnv.height = 256;
+    //    cnv.width = 240;
+//    cnv.width = 512;
+//    cnv.height = 240;
+    
+    if (elem.requestFullscreen) {
+	elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+	elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+	elem.msRequestFullscreen();
+    }
+}
+
 function animation() {
     ctx.fillStyle = "rgb(147,187,236)";
     ctx.clearRect(0, 0, cnv.width, cnv.height);    
@@ -21,3 +41,5 @@ btnDe.addEventListener("touchend", function (e) {movDe=false;det=true;}, false);
 var btnJump = document.getElementById("jump");
 btnJump.addEventListener("touchstart", function (e) {jump=true;}, false);
 btnJump.addEventListener("touchend", function (e) {jump=false;}, false);
+
+
